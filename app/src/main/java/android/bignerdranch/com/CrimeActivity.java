751b113826a081807da7521a8804ddd861dd3adc
herbertflores <h_f015@student.usc.edu.au>
2019-08-21 15:39:43 +1000

    CrimeListActivity Done

---
+++ app/src/main/java/android/bignerdranch/com/CrimeActivity.java
@@ -0,0 +1,15 @@
+package android.bignerdranch.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class CrimeActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+
+}
---
+++ app/src/main/java/android/bignerdranch/com/CrimeListFragment.java
@@ -0,0 +1,7 @@
+package android.bignerdranch.com;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeListFragment extends Fragment {
+    // nothing yet
+}
